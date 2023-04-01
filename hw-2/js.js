8d863a81099028f6f8318ea19f4a2fff7c6450f1
Vladimir96Nikiforov <vnikiforov96@mail.ru>
2023-04-01 17:02:19 +0300

    2-nd course hw

---
+++ hw-2/js.js
@@ -0,0 +1,48 @@
+let a = 10;
+a = 20;
+alert(a);
+
+let iphone = 2007;
+alert(iphone);
+
+let nameJs = 'Брендан Эйх';
+alert(nameJs);
+
+let two = 2;
+let ten = 10;
+alert([ten + two , ten - two , ten * two, ten / two]);
+
+let result = 2**5;
+alert(result);
+
+let aa = 9;
+let b = 2;
+alert(aa%b);
+
+
+let num = 1;
+num +=5;
+num -=3;
+num *=7;
+num /=3;
+num++;
+num--;
+alert(num);
+
+
+let age = prompt("Сколько вам лет?");
+alert(age);
+let user = {     // объект
+    name: "John",  // под ключом "name" хранится значение "John"
+    age: 30,        // под ключом "age" хранится значение 30
+    isAdmin: false,
+    cityOfResidence: "MSK",
+    age:44,
+    info: prompt("Какую информацию хотите узнать o пользователе?") 
+    };
+delete user.cityOfResidence;
+alert(user.info)
+console.log(user);
+
+let userName = prompt("Ваше имя?")
+alert(["Привет, ", userName])
