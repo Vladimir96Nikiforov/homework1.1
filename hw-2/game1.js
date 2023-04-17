bf7facea79f655b33829e95a18066bc74a460ba1
Vladimir96Nikiforov <vnikiforov96@mail.ru>
2023-04-17 23:15:41 +0300

    исправление hw5

--- hw-2/game1.js
+++ hw-2/game1.js
@@ -4,24 +4,25 @@ switch (month) {
 	case 12 :
     case 1 :
     case 2 :
-		console.log('Зима');
+		alert('Зима');
 		break;
 	case 3 :
     case 4 :
     case 5 :
-		console.log('Весна');
+		alert('Весна');
 		break;
     case 6 :
     case 7 :
     case 8 :
-		console.log('Лето');
+		alert('Лето');
 		break;
     case 9 :
     case 10 :
     case 11 :
-		console.log('Осень');
+		alert('Осень')
 		break;
-	default: console.log('Такого сезона нет');
+		
+	default: alert('Такого сезона нет');
 		break;
 }
 }
--- hw-2/js4.js
+++ hw-2/js4.js
@@ -1,30 +1,27 @@
+// Задание 1
 const count = function (a,b) {
     a = 10;
     b = 7;
-	if (a > b) {
-        console.log(b);
+    if (a > b) {
+        return b
     }
-    else if (b > a){
-        console.log(a);
-    }
-    else {
-        console.log(b);
-    }
+    else return a
 };
 count();
 
-
+// Задание 2
 const rem = function (n) {
     n = 8
     if (n % 2 == 0){
-        console.log('Четное');
+        return('Четное');
     }
     else {
-        console.log('Нечетное');
+        return('Нечетное');
     }
 };
 rem();
 
+// Задание 3
 const upTwo = function (x){
     x = 12;
     console.log(Math.pow(x,2))
@@ -37,13 +34,13 @@ upTwo()
 //     console.log(Math.pow(c, 2))
 // }
 
-
+// Задание 4
 const age = function (){
     let n = prompt ('Введите ваш возраст'); 
-    if (n < 0){
+    if (n <= 0){
         alert('Вы ввели неправильное значение')
     }
-    else if (n < 12 && n > 0){
+    else if (n <= 12 && n > 0){
         alert('Привет друг')
     }
     else {
@@ -69,30 +66,45 @@ age()
 // }
 // numFunc()
 
+
+// Задание 5
 function myFunction(){
     let firstNum = prompt ('Число один');
     let secondNum = prompt ('Число два');
     let res = "";
     res = Number.isNaN(firstNum || secondNum)
     if (res === false){
-        alert(firstNum * secondNum)
+        return(firstNum * secondNum)
     }
     else {
-        alert('Одно или оба значения не являются числом');
+        return('Одно или оба значения не являются числом');
     }
 }
 myFunction()
 
+// Задание 6
 
+// function myFunctionMult(){
+//     let threeMult = prompt ('Введите число');
+//     let res = "";
+//     res = Number.isNaN(threeMult)
+//     if (res === false){
+//         return(Math.pow(threeMult, 3))
+//     }
+//     else {
+//         return('ошибка');
+//     }
+// }
+// myFunctionMult()
 function myFunctionMult(){
     let threeMult = prompt ('Введите число');
     let res = "";
     res = Number.isNaN(threeMult)
     if (res === false){
-        alert(Math.pow(threeMult, 3))
+        return('n в кубе равняется'(Math.pow(threeMult, 3)))
     }
     else {
-        alert('ошибка');
+        return('ошибка');
     }
 }
 myFunctionMult()
@@ -118,24 +130,77 @@ myFunctionMult()
 //     }
 //     getArea()
 
-let circle1 = {
-radius: 27,
-}
-circle1.getArea = function (){
-f = this.radius * this.radius * 3.14;
-p = this.radius * 3.14 * 2;
-console.log(f);
-console.log(p);
-}
-circle1.getArea()
 
-let circle2 = {
-    radius: 7,
-}
-circle2.getArea = function (){
-f = this.radius * this.radius * 3.14;
-p = this.radius * 3.14 * 2;
-console.log(f);
-console.log(p);
-}
-circle2.getArea()
+// Задание 7
+
+// let circle1 = {
+// radius: 27,
+// }
+// circle1.getArea = function (){
+// f = this.radius * this.radius * 3.14;
+// p = this.radius * 3.14 * 2;
+// console.log(f);
+// console.log(p);
+// }
+// circle1.getArea()
+
+// let circle2 = {
+//     radius: 7,
+// }
+// circle2.getArea = function (){
+// f = this.radius * this.radius * 3.14;
+// p = this.radius * 3.14 * 2;
+// console.log(f);
+// console.log(p);
+// }
+// circle2.getArea()
+
+
+
+let circle1 = {
+    radius: 27,
+    }
+    circle1.getArea = function (){
+    f = this.radius * this.radius * 3.14;
+    p = this.radius * 3.14 * 2;
+    return(f,p);
+    }
+    console.log( circle1.getArea() );
+    circle1.getArea()
+    
+    let circle2 = {
+        radius: 7,
+    }
+    circle2.getArea = function (){
+    f = this.radius * this.radius * 3.14;
+    p = this.radius * 3.14 * 2;
+    return(f,p);
+    }
+    console.log( circle1.getArea() );
+    circle2.getArea()
+
+
+
+// function getRectangleArea () {
+//     return Math.PI * Math.pow(this.radius, 2);
+// }
+// function getRectanglePerimeter () {
+//     return 2 * Math.PI * this.radius;
+// }
+
+// const circle1 = {
+//     radius: 10,
+//     getArea: getRectangleArea,
+//     getPerimeter: getRectanglePerimeter,
+// }
+
+// const circle2 = {
+//     radius: 20,
+//     getArea: getRectangleArea,
+//     getPerimeter: getRectanglePerimeter,
+// }
+
+// console.log(`Площадь первой окружности равна ${circle1.getArea()}`);
+// console.log(`Периметр первой окружности равен ${circle1.getPerimeter()}`);
+// console.log(`Площадь второй окружности равна ${circle2.getArea()}`);
+// console.log(`Периметр второй окружности равен ${circle2.getPerimeter()}`);
